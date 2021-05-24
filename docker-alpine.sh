36f6bfd9faf83c2b12e040c5f595925671ad3321
harshad <harshad@squashfs-root>
2021-05-24 22:01:06 +0530

    Changed image name - from vm

--- docker-alpine.sh
+++ docker-alpine.sh
@@ -13,4 +13,4 @@ docker build --rm --no-cache \
     --build-arg BUILD_DATE="$(date +"%Y-%m-%dT%H:%M:%SZ")" \
     --build-arg TAG_SUFFIX=default \
     --file Dockerfile.custom \
-    --tag stable:node-red-build .
+    --tag stable:nodered .
